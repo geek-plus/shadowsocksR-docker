17542fd57688dc85e0dd405c988f701394521924
wan-qy <wan-qy@users.noreply.github.com>
2016-05-08 07:26:41 -0700

    Add files.

---
+++ Dockerfile
@@ -0,0 +1,25 @@
+FROM ubuntu:xenial
+MAINTAINER slanterns <slanterns.w@gmail.com>
+
+RUN apt update
+RUN apt upgrade -y
+#当心缓存机制
+
+#基本库安装
+RUN apt install m2crypto git -y
+RUN apt install build-essential -y
+ADD https://github.com/jedisct1/libsodium/releases/download/1.0.10/libsodium-1.0.10.tar.gz /
+RUN ./configure && make -j2 && make install
+RUN ldconfig
+
+#获取源代码
+RUN mkdir shadowsocksR && cd shadowsockR
+RUN git clone -b manyuser https://github.com/breakwa11/shadowsocks.git
+
+#服务端配置
+COPY configurer run.sh /shadowsocksR/
+RUN chmod +x run.sh
+
+CMD ["./run.sh"]
+
+#Thank clowwindy,breakwa11,PeterCxy and all shadowsocks project maintains.
---
+++ configurer.pas
@@ -0,0 +1,35 @@
+var
+server_port,password,timeout,method,protocol,obfs,redirect,dns_ipv6,fast_open,workers:string;
+begin
+assign(output,'/etc/shadowsocks.json');
+rewrite(output);      //1、防止反复在文件尾写入；2、防止环境变量更改后配置文件不改变；
+//以下均不能读入空串！（输入一个空串=未输入）
+readln(server_port);  //e.g. 8388,与容器启动参数中的-p <CONTAINER_PORT> 或 -p <HOT_PORT>:<CONTAINER_PORT>一致
+readln(password);     //e.g. mypassword
+readln(timeout);      //e.g. 120
+readln(method);       //e.g. aes-256-cfb
+readln(protocol);     //e.g. auth_sha1
+readln(obfs);         //e.g. tls1.2_ticket_auth
+readln(redirect);     //e.g. 值为空字符串或一个列表，若为列表示例如"redirect":["bing.com", "cloudflare.com:443"],示例:"bing.com", "cloudflare.com:443"或"";作用是在连接方的数据不正确的时候，把数据重定向到列表中的其中一个地址和端口（不写端口则视为80），以伪装为目标服务器。即使不使用重定向，也要填写""
+readln(dns_ipv6);     //e.g. true or false
+readln(fast_open);    //e.g. true or false
+readln(workers);      //e.g. 1
+writeln('{');
+writeln('    "server": "0.0.0.0",');
+writeln('    "server_ipv6": "::",');
+writeln('    "server_port": ',server_port,',');
+writeln('    "local_address": "127.0.0.1",');
+writeln('    "local_port": 1080,');
+writeln('    "password": "',password,'",');
+writeln('    "timeout": ',timeout,',');
+writeln('    "method": "',method,'",');
+writeln('    "protocol": "',protocol,'",');
+writeln('    "protocol_param": "",');
+writeln('    "obfs": "',obfs,'",');
+writeln('    "obfs_param": "",');
+writeln('    "redirect": ',redirect,',');
+writeln('    "dns_ipv6": ',dns_ipv6,',');
+writeln('    "fast_open": ',fast_open,',');
+writeln('    "workers": ',workers,'');
+write('}');
+end.
