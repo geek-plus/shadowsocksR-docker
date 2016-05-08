9cf03bad752ba45c32cfe48bd645fe7e526b7d2b
wan-qy <wan-qy@users.noreply.github.com>
2016-05-09 00:51:13 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd shadowsocksR
-echo -e "$password/n$method/n$protocol/n$obfs/n$redirect/n$dns_ipv6/" | ./configurer
+echo -e "$password\n$method\n$protocol\n$obfs\n$redirect\n$dns_ipv6" | ./configurer
 #此实现：每一项后换行，在程序中使用readln(); readln();...readln(); 读入；
 #另一种实现：每一项后空格，程序中使用read()；读入一个长字符串，再根据空格处理出每一项。
 
