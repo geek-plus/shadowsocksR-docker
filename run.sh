a3f9789956a1a1ae72655a32a71937af3cb51c15
wan-qy <wan-qy@users.noreply.github.com>
2016-05-09 19:05:34 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-cd shadowsocksR
-bash -c echo -e "$password\n$method\n$protocol\n$obfs\n$redirect\n$dns_ipv6" | ./configurer
+bash -c echo -e "$password\n$method\n$protocol\n$obfs\n$redirect\n$dns_ipv6" | /shadowsocksR/configurer
 #此实现：每一项后换行，在程序中使用readln(); readln();...readln(); 读入；
 #另一种实现：每一项后空格，程序中使用read()；读入一个长字符串，再根据空格处理出每一项。
 
-cd /shadowsocks/shadowsocks/ && python server.py -c /shadowsocksR/shadowsocks/shadowsocks/shadowsocks.json
+python /shadowsocksR/shadowsocks/shadowsocks/server.py -c /shadowsocksR/shadowsocks/shadowsocks/shadowsocks.json
